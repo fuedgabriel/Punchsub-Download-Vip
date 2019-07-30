471977b6c5a108e71c89d2bda85d0ca19afaa79d
Fued Gabriel <fuedgabriel@hotmail.com>
2019-07-29 21:54:58 -0300

    Update PunchSub.py

--- PunchSub.py
+++ PunchSub.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import re
+import wget
 #login
 login = 'fuedgabriel'
 password = 'fuedqe2902'
@@ -12,9 +13,7 @@ link = 'https://punchsubs.net/buscar-projeto/anime'
 #Contadores
 
 
-#abrir lista de animes
-f = open('links.json', 'r')
-f = f.read().split('}')
+
 
 driver = webdriver.Chrome()
 
@@ -30,14 +29,15 @@ def Login():
     AllAnimeSolo()
 
     
+    
 def AllAnimeSolo():
-    colum = 0
-    cont = 0
-    cja = 0
-    #CApturando vetor
+    #abrir lista de animes
+    f = open('links.json', 'r')
+    f = f.read().split('}')
+    
+    #Capturando vetor
     for col in f:
         col = col.split(',')
-        #cont = cont +1
         a = 12
         x = '' 
 
@@ -45,52 +45,89 @@ def AllAnimeSolo():
         while('"' not in x):
             x = col[1][11:a]
             a+=1
-
-
-        print('Nome do anime: '+x)    
+        print('Nome do anime: '+x)
+        
         #numero de episodeos
         resp = re.compile('[0-9]+').findall(col[2])
         resp = resp[0]
         print('Número de episodeos: '+ str(resp))
 
+
     
         for Collum in range(20):
             if('link' in col[Collum]):
-                print('Link de download: ' + col[Collum][10:100].replace('"','').replace('\\',''))
-                print('Link de Imagem: '+ col[Collum+1][10:100].replace('"','').replace('\\',''))
-                time.sleep(2)
+                #pegar link de download, imagem e abrir o link do anime
                 link = 'https://punchsubs.net/'+col[Collum][10:100].replace('"','').replace('\\','')
-                driver.get(link)
+                print('Link de download: ' + link)
+                print('Link de Imagem: '+ col[Collum+1][10:1000].replace('"','').replace('\\',''))
+
+                
+                #abrir link do anime
+                driver.get('https://punchsubs.net/projeto/2525/aico-incarnation')
                 id = driver.page_source
                 sopa = BeautifulSoup(id, 'html.parser')
-                cja+=1
                 print()
                 print()
                 print()
-                print(cja)
                 print()
                 print()
-                print()
-                break
+                
+                #break
+
+                #capturar link por link
                 img = 0
-                '''
                 test = sopa.find_all("img", class_='card-img-top rounded screen')
 
                 for clicks in test:
                     img+=1
-                    print(clicks)
+                    print('link das imagens dos episodeos: '+ str(clicks))
+                    print()
+                    print()
                     
                     driver.find_element(By.XPATH, '//*[@id="cards-episodios"]/ul[1]/li['+str(img)+']/div[1]/img').click()
+                    time.sleep(0.5)
+                    driver.find_element(By.XPATH, '//*[@id="nav-sd-tab"]').click()
                     
-                    driver.find_element(By.XPATH, '//*[@id="nav-fullhd-tab"]').click()
-                    driver.find_element(By.XPATH, '//*[@id="mirror-vip"]').click()
+                    id = driver.page_source
+                    sopa = BeautifulSoup(id, 'html.parser')
+                    download = sopa.find_all("a", class_='link-mirror')
+                    f = 0
+                    for g in download:
+                        if('vip' in str(download[f]) and 'http:' in str(download[f])):
+                            print()
+                            print('Link verdadeiro')
+                            if(f == 0 or f == 7):
+                                print('muito grande')
+                                print(str(f))
+                                print()
+                            else:
+                                print('efetuando download')
+                                href = download[f].get('href')
+                                wget.download(href)
+                        else:
+                            print()
+                            print('falso')
+                            print()
+                        f+=1
+                    driver.find_element(By.XPATH, '//*[@id="metodosDownloadModal"]/div/div/div[1]/button').click()
                     
+                    
+                    
+                    '''
                     driver.find_element(By.XPATH, '//*[@id="nav-hd-tab"]').click()
-                    driver.find_element(By.XPATH, '//*[@id="mirror-vip"]').click()
+                    id = driver.page_source
+                    sopa = BeautifulSoup(id, 'html.parser')
+                    zip_link = sopa.find_all("a", class_='link-mirror')
+                    zip = zip_link[0].get('href')
+                    wget.download(zip)
                     
                     driver.find_element(By.XPATH, '//*[@id="nav-sd-tab"]').click()
-                    driver.find_element(By.XPATH, '//*[@id="mirror-vip"]').click()
-'''
+                    id = driver.page_source
+                    sopa = BeautifulSoup(id, 'html.parser')
+                    zip_link = sopa.find_all("a", class_='link-mirror')
+                    zip = zip_link[0].get('href')
+                    wget.download(zip)
+                    '''
                     
                     
                 
